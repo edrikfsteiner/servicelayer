bc530e1c38061c81aeaffc0f8c098c7bfc1987ba
edrikfsteiner <edrikfsteiner@gmail.com>
2026-03-14 16:19:02 -0300

    Configuração do application.yaml e dos bancos mysql e postgresql

---
+++ src/main/java/com/migration/servicelayer/config/OriginDbConfig.java
@@ -0,0 +1,53 @@
+package com.migration.servicelayer.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "originEntityManagerFactory",
+        transactionManagerRef = "originTransactionManager",
+        basePackages = {"com.migration.servicelayer.mysql.repository"}
+)
+public class OriginDbConfig {
+
+    @Bean(name = "originDataSource")
+    @ConfigurationProperties(prefix = "app.datasource.origin")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "originEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(@Qualifier("originDataSource") DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setDataSource(dataSource);
+        entityManagerFactory.setPackagesToScan("com.migration.servicelayer.mysql.entity");
+        entityManagerFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", "none");
+        entityManagerFactory.setJpaPropertyMap(properties);
+
+        return entityManagerFactory;
+    }
+
+    @Bean(name = "originTransactionManager")
+    public PlatformTransactionManager transactionManager(
+            @Qualifier("originEntityManagerFactory") LocalContainerEntityManagerFactoryBean entityManagerFactory
+    ) {
+        return new JpaTransactionManager(entityManagerFactory.getObject());
+    }
+}
---
+++ src/main/java/com/migration/servicelayer/config/TargetDbConfig.java
@@ -0,0 +1,57 @@
+package com.migration.servicelayer.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "targetEntityManagerFactory",
+        transactionManagerRef = "targetTransactionManager",
+        basePackages = {"com.migration.servicelayer.postgresql.repository"}
+)
+public class TargetDbConfig {
+
+    @Primary
+    @Bean(name = "targetDataSource")
+    @ConfigurationProperties(prefix = "app.datasource.target")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean(name = "targetEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(@Qualifier("targetDataSource") DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setDataSource(dataSource);
+        entityManagerFactory.setPackagesToScan("com.migration.servicelayer.postgresql.entity");
+        entityManagerFactory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", "validate");
+        entityManagerFactory.setJpaPropertyMap(properties);
+
+        return entityManagerFactory;
+    }
+
+    @Primary
+    @Bean(name = "targetTransactionManager")
+    public PlatformTransactionManager transactionManager(
+            @Qualifier("targetEntityManagerFactory") LocalContainerEntityManagerFactoryBean entityManagerFactory
+    ) {
+        return new JpaTransactionManager(entityManagerFactory.getObject());
+    }
+}
