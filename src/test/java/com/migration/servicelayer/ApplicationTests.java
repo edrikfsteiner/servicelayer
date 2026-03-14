37300e78a12e1737853459452c372118d73082e9
edrikfsteiner <edrikfsteiner@gmail.com>
2026-03-14 11:42:49 -0300

    First commit

---
+++ src/test/java/com/migration/servicelayer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migration.servicelayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
