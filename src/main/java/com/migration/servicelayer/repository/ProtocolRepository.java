6458aa4d10a55a3d68956b412287f968f74cdc53
edrikfsteiner <edrikfsteiner@gmail.com>
2026-04-04 16:11:01 -0300

    Trocado lakehouse de postgresql para mongo

--- src/main/java/com/migration/servicelayer/repository/ProtocolRepository.java
+++ src/main/java/com/migration/servicelayer/repository/ProtocolRepository.java
@@ -1,7 +1,6 @@
 package com.migration.servicelayer.repository;
 
 import com.migration.servicelayer.model.IngestionProtocol;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
